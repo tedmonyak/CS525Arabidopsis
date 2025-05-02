c656a7fd532d791f0deb28eb945f14436cc56f25
formanj-0 <formanj@colostate.edu>
2025-05-02 09:42:37 -0600

    Loader is ready

--- Load_Data.py
+++ Load_Data.py
@@ -36,8 +36,8 @@ def one_hot_encode(seq):
 def load_data(data_dir:str, val_chr:str='Chr5', training_testing_split:float=0.7, 
               test_training_data_to_load:float=500, val_data_to_load:float=100):
     # get the .fasta files in data_dir
-    fasta_files = [f for f in os.listdir(data_dir) if 'fasta' in f]
-    faste_files = [f for f in os.listdir(data_dir) if 'faste' in f]
+    fasta_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if 'fasta' in f]
+    faste_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if 'faste' in f]
 
 
     # find the parts of the fasta files that is apart of val_chr
@@ -53,7 +53,6 @@ def load_data(data_dir:str, val_chr:str='Chr5', training_testing_split:float=0.7
     testing_training_index = testing_training_index[:min(test_training_data_to_load, len(testing_training_index))]
     val_indices = val_indices[:min(val_data_to_load, len(val_indices))]
 
-
     # Load the the sequences
     X_val = []
     with open(fasta_files[0], "rt") as handle:
@@ -68,23 +67,36 @@ def load_data(data_dir:str, val_chr:str='Chr5', training_testing_split:float=0.7
                 X_training_testing.append(one_hot_encode(record.seq))
 
     Y_val = []
-    with open(faste_files[0], "rt") as handle:
-        for i, record in enumerate(SeqIO.parse(handle, "fasta")):
-            if i in val_indices:
-                Y_val.append(np.array([eval(str(s)) for s in record.seq.split(',')]))
+    for s in faste_files:
+        Y = []
+        with open(faste_files[0], "rt") as handle:
+            for i, record in enumerate(SeqIO.parse(handle, "fasta")):
+                if i in val_indices:
+                    Y.append(np.array([eval(str(s)) for s in record.seq.split(',')]))
+        Y_val.append(Y)
 
     Y_training_testing = []
-    with open(faste_files[0], "rt") as handle:
-        for i, record in enumerate(SeqIO.parse(handle, "fasta")):
-            if i in testing_training_index:
-                Y_training_testing.append(np.array([eval(str(s)) for s in record.seq.split(',')]))
+    for s in faste_files:
+        Y = []
+        with open(s, "rt") as handle:
+            for i, record in enumerate(SeqIO.parse(handle, "fasta")):
+                if i in testing_training_index:
+                    Y.append(np.array([eval(str(s)) for s in record.seq.split(',')]))
+        Y_training_testing.append(Y)
     
+    # Reorder Y from (Tissue, Seq) -> (Seq, Tissue)
+    Y_training_testing = np.array(Y_training_testing)
+    Y_val = np.array(Y_val)
+    Y_training_testing = np.swapaxes(Y_training_testing, 0, 1)
+    Y_val = np.swapaxes(Y_val, 0, 1)
+
+    X_val = np.array(X_val)
+    X_training_testing = np.array(X_training_testing)
 
     # Split the trainig and testing data
     x_train, x_test, y_train, y_test = train_test_split(
         X_training_testing, Y_training_testing, test_size=(1 - training_testing_split), random_state=42
     )
-
 
     # put the data into a dataloader
     training_dataset = chromatin_dataset([(x,y) for x,y in zip(x_train, y_train)])
@@ -95,8 +107,7 @@ def load_data(data_dir:str, val_chr:str='Chr5', training_testing_split:float=0.7
 
 
 if __name__ == '__main__':
-    cwd = os.getcwd()
-    training_dataset, testing_dataset, validation_dataset = load_data(cwd)
+    training_dataset, testing_dataset, validation_dataset = load_data(os.path.join(os.getcwd(), 'Data', 'Parsed_Data'), test_training_data_to_load=10, val_data_to_load=10)
     print(len(training_dataset))
     print(len(testing_dataset))
     print(len(validation_dataset))
